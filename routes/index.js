21d9ca55493a55f0dbd61ae9aee48f4ac1f6ebd0
japan095 <sanglaocong@gmail.com>
2021-10-18 20:36:01 +0700

    CongNgheMoi_Proj

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "database-1.chgp8y6tdnwl.ap-southeast-1.rds.amazonaws.com",
+  user: "sa",
+  password: "sapassword",
+  database: "gym"
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  //Select only "name" and "address" from "customers":
+  con.query("SELECT * FROM courses", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result[0].name);
+    /* GET home page. */
+    router.get('/', function (req, res, next) {
+      res.render('index',{data:result});
+    });
+  });
+});
+
+
+
+
+module.exports = router;
